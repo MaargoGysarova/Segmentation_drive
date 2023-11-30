26533ca4cfa81630390f622bbd344908179ea13f
Маргарита <113328619+MaargoGysarova@users.noreply.github.com>
2023-11-30 14:00:09 +0400

    написан класс для работы с датасет

---
+++ work_with_dataset.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class MyDataset(Dataset):
+    def __init__(self, csv_path, img_dir, mask_dir,  transform=None):
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+
+        self.images = os.listdir(img_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir, self.images.iloc[index, 0])
+        mask_path = os.path.join(self.mask_dir, self.images.iloc[index, 1].replace('.png', '_L.png'))
+
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+
+        mask[mask == 255.0] = 1.0
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+        
+        return image, mask
